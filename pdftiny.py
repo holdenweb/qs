9ea8af7967d45eaabbbf60e6b8ce6c993e0dbdb7
Steve Holden <steve@holdenweb.com>
2022-06-07 14:15:15 +0100

    Establish basic label format.

--- pdftiny.py
+++ pdftiny.py
@@ -3,15 +3,11 @@ from reportlab.lib.units import inch
 import qrcode
 
 def hello(c):
-    for x in range(27):
-        for y in range(10):
-            x, y = x*10, y*10
-            c.line(0, y, 270, y)
-            c.line(x, 0, x, 200)
-    c.drawString(20,20,"Hello World")
-    qc = qrcode.make("https://holdenweb.com/")
-    qc.save("qrcode.gif")
-    c.drawImage("qrcode.gif", 190, 40, width=50, height=50)
+    c.rect(20, 10, 222, 83)
+    c.setFont("Times-Bold", 18)
+    c.drawString(25, 72, "Hello World")
+    qc = qrcode.make("https://holdenweb.com/parts/{id}")
+    c.drawInlineImage(qc, 190, 40, width=50, height=50)
 
 c = canvas.Canvas("hello.pdf", pagesize=(3.5*inch, 1.4*inch))
 hello(c)
