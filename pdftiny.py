a00352f6601c6ce4e99329b9b1204b8e3b5e44a4
Steve Holden <steve@holdenweb.com>
2023-03-03 16:18:53 +0000

    djust sample text and font sizes.

--- pdftiny.py
+++ pdftiny.py
@@ -1,23 +1,27 @@
+from io import BytesIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 import qrcode
 
 data = """\
-Part Name
-Some sort of descriptive text
-Another line of text
-Another of these endless lines of text ...
-and this is to see what the whole schemaozzle looks like""".splitlines()
+Part Name and/or
+Descriptive Title
+Another line of text in Helvetica
+Another of these endless lines of text
+to see what the whole schemozzle looks like""".splitlines()
 
-fonts = ["Times-Bold"] + 3*["Helvetica"] + ["Courier"]
-sizes = [18, 12, 12, 12, 6]
-linepos = list(range(72, 10, -15))
-c = canvas.Canvas("hello.pdf", pagesize=(3.5*inch, 1.4*inch))
+fonts = ["Times-Bold"]*2 + ["Helvetica"] + ["Courier"]*2
+sizes = [18, 18, 11, 8, 8]
+linepos = [77 - n for n in (0, 20, 36, 52, 62)]
+c = canvas.Canvas(file := BytesIO(),  pagesize=(3.5*inch, 1.4*inch))
 c.rect(20, 10, 222, 83)
 qc = qrcode.make("https://holdenweb.com/parts/{id}")
-c.drawInlineImage(qc, 190, 40, width=50, height=50)
+c.drawInlineImage(qc, 180, 30, width=60, height=60)
 for font, size, pos, line in zip(fonts, sizes, linepos, data):
     c.setFont(font, size)
     c.drawString(25, pos, line)
 c.showPage()
 c.save()
+with open("1hello.pdf", "wb") as out_file:
+    out_file.write(file.getvalue())
+
