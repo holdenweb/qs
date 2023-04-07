5c0ce55310018516fbd9b69f02c090920c769299
Steve Holden <steve@holdenweb.com>
2023-04-07 13:24:03 +0100

    Add the opalsync command to refresh apps from Opalstack.

---
+++ src/qs/opalsync.py
@@ -0,0 +1,28 @@
+"""
+sync.py: Download the Opalstack state.
+"""
+import os
+import sys
+
+from mongoengine import connect
+from models import Application
+from hu import ObjectDict as OD
+import opalstack as ops
+
+MANAGER_NAME = "sholden"
+SERVER_NAME = "opal5.opalstack.com"
+
+token = os.getenv('OPALSTACK_TOKEN')
+api = ops.Api(token=token)
+a_mgr = ops.api.AppsManager(api)
+
+def main():
+    print("Yeah!")
+    connect("opalstack")
+    app_list = a_mgr.list_all()
+    for app in app_list:
+        Application(**app).save()
+
+
+if __name__ == '__main__':
+    main()
