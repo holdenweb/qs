54b1ea50f52e719779262ae9778bdf6a31ac0dff
Steve Holden <steve@holdenweb.com>
2023-04-12 08:35:41 +0100

    Add domains and sites with partially-specified models.

--- src/qs/models.py
+++ src/qs/models.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, DictField, StringField, IntField, BooleanField, UUIDField, URLField
+from mongoengine import Document, DynamicDocument, DictField, ListField, StringField, IntField, BooleanField, UUIDField, URLField
 
 class Application(Document):
     id = UUIDField(primary_key=True)
@@ -12,3 +12,27 @@ class Application(Document):
     installer_url = URLField(required=False)
     json = DictField()
     osuser_name = StringField()
+
+class Domain(Document):
+    id = UUIDField(primary_key=True)
+    state = StringField()
+    ready = BooleanField()
+    name = StringField(unique=True)
+    dkim_record = StringField()
+    is_valid_hostname = BooleanField()
+
+class Site(DynamicDocument):
+    id = UUIDField(primary_key=True)
+    state = StringField()
+    ready = BooleanField()
+    name = StringField(unique=True)
+    server = UUIDField()
+    ip4 = UUIDField()
+    ip6 = UUIDField(required=False)
+    disabled = BooleanField()
+    domains = ListField(UUIDField())
+    # routes = ListField
+    generate_le = BooleanField()
+    cert = UUIDField()
+    redirect = BooleanField()
+
--- src/qs/opalsync.py
+++ src/qs/opalsync.py
@@ -5,9 +5,10 @@ import os
 import sys
 
 from mongoengine import connect
-from qs.models import Application
+from qs.models import Application, Domain, Site
 from hu import ObjectDict as OD
-import opalstack as ops
+import opalstack as ostack
+
 
 MANAGER_NAME = "sholden"
 SERVER_NAME = "opal5.opalstack.com"
@@ -15,16 +16,21 @@ SERVER_NAME = "opal5.opalstack.com"
 token = os.getenv('OPALSTACK_TOKEN', None)
 if not token:
     sys.exit("OPALSTACK_TOKEN not found in environment")
-api = ops.Api(token=token)
-a_mgr = ops.api.AppsManager(api)
+api = ostack.Api(token=token)
+a_mgr = ostack.api.AppsManager(api)
+d_mgr = ostack.api.DomainsManager(api)
+s_mgr = ostack.api.SitesManager(api)
 
 def main():
-    print("Yeah!")
     connect("opalstack")
     app_list = a_mgr.list_all()
     for app in app_list:
         Application(**app).save()
-
-
+    domain_list = d_mgr.list_all()
+    for domain in domain_list:
+        Domain(**domain).save()
+    site_list = s_mgr.list_all()
+    for site in site_list:
+        Site(**site).save()
 if __name__ == '__main__':
     main()
