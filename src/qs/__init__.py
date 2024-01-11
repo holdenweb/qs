3148468de7e113e67d0276ef1db1726999996605
Steve Holden <steve@holdenweb.com>
2024-01-11 15:24:54 +0000

    Add debug printing (complete with recursion bug).

--- src/qs/__init__.py
+++ src/qs/__init__.py
@@ -9,25 +9,32 @@ from jinja2 import Environment, PackageLoader
 import logging
 logging.basicConfig(level=logging.INFO)
 
+DEBUG = True
 HOSTS = ['opal5.opalstack.com']
 conn = connect('opalstack')
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 @task(hosts=HOSTS)
 def deploy(c, app, version):
+
+    def crun(cmd):
+        if DEBUG:
+            print("+", cmd)
+        return c.run(cmd)
+
     print(f"Deploying with qs {__version__}")
     try:
         app = App.objects.get(name=app)
     except App.DoesNotExist:
         sys.exit(f"Application {app!r} not known: do you need to run sync-apps?")
-    # loader = FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")
     loader = PackageLoader('qs', 'templates')
     jenv = Environment(
         loader=loader,
         autoescape=False
     )
+
     if not app.port:
         sys.exit("App has no port number: please re-sync by running sync-apps.")
     for filename in ('kill', 'start', 'stop', 'uwsgi.ini'):
@@ -37,13 +44,14 @@ def deploy(c, app, version):
             f.write(content)
             c.local(f'echo {version} > version.txt')
     c.local(fr'(tar cf release-{version}.tgz --exclude __pycache__ --exclude \*.DS_Store --exclude \*.tgz --exclude .git\* .)')
+
     with c.cd(f"apps/{app.name}"):
-        c.run("./stop || echo Not running")
-        c.run(f'mkdir -p apps/{version} envs/{version} tmp && rm -rf tmp/* ')
+        crun("./stop || echo Not running")
+        crun(f'mkdir -p apps/{version} envs/{version} tmp && rm -rf tmp/* ')
         Transfer(c).put(f'release-{version}.tgz', f'apps/{app.name}')
-        c.run(f"cd apps/{version} && tar xf ../../release-{version}.tgz && cp kill start stop uwsgi.ini ../..")
-        c.run(f"rm -rf myapp env && ln -sF apps/{version} myapp && ln -sF envs/{version} env")
-        c.run(f"""\
+        crun(f"cd apps/{version} && tar xf ../../release-{version}.tgz && cp kill start stop uwsgi.ini ../..")
+        crun(f"rm -rf myapp env && ln -sF apps/{version} myapp && ln -sF envs/{version} env")
+        crun(f"""\
 chmod +x kill start stop &&
 python3.10 -m venv envs/{version} &&
 source envs/{version}/bin/activate &&
@@ -52,7 +60,9 @@ rm -f myapp ; ln -s apps/{version} myapp &&
 rm -f env ; ln -s envs/{version} env &&
 ln -sf /home/sholden/bin/uwsgi env/bin
 """)
-        c.run("pwd && ./start")
+        crun("pwd && ./start")
+
+    c.close()
 
 
 def main():
