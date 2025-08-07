5b0c56aa242c4b95d7fa2bca3a62a1c6e947096b
Steve Holden <steve@holdenweb.com>
2025-08-07 11:39:36 +0100

    Make directories as needed; import from project, not app.

--- src/qs/__init__.py
+++ src/qs/__init__.py
@@ -72,14 +72,14 @@ def deliver(c, app, version):
 
     if not app.port:
         sys.exit("App has no port number: please re-sync by running opalsync.")
-    create_wsgi(name=app.name, port=app.port)
+    create_wsgi(name=mod_name, port=app.port)
 
     c.local(f'echo {version} > version.txt')
     cmd = fr'(gtar cf {proj_name}-{version}.tgz --no-xattrs -T Manifest.txt wsgi.py dist/{proj_name}-{version}-py3-none-any.whl)'
     c.local(cmd)
     with c.cd(f"apps/{app.name}"):
         remote("mkdir -p html md apps dist envs releases wsgis")
-    Transfer(c).put(f'{proj_name}-{version}.tgz', f'apps/{app.name}/releases/{proj_name}-{version}.tgz')
+        Transfer(c).put(f'{proj_name}-{version}.tgz', f'releases/{proj_name}-{version}.tgz')
     cmd = f"ensconce {app.name} {proj_name} {version}"
     remote(cmd)
 
