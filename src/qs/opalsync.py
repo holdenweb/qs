62a9b2272270a041f052258506ada76fd2a3786d
Steve Holden <steve@holdenweb.com>
2025-08-07 12:11:31 +0100

    Drop a collection before downloading data to avoid duplicate keys.

--- src/qs/opalsync.py
+++ src/qs/opalsync.py
@@ -5,8 +5,7 @@ import os
 import sys
 
 from mongoengine import connect
-from qs.models import App, Domain, Site, class_for
-from hu import ObjectDict as OD
+from qs.models import class_for
 import opalstack as ostack
 
 ostack.apps
@@ -19,24 +18,26 @@ token = os.getenv('OPALSTACK_TOKEN', None)
 if not token:
     sys.exit("OPALSTACK_TOKEN not found in environment")
 
-object_types = ['Accounts', 'Addresses', 'Apps', 'Certs', 'Dnsrecords', 'Domains', 'Ips', 'Mailusers',
+object_type_names = ['Accounts', 'Addresses', 'Apps', 'Certs', 'Dnsrecords', 'Domains', 'Ips', 'Mailusers',
                 'Mariadbs', 'Mariausers', 'Notices', 'OSUsers', 'OSVars', 'Psqldbs', 'Psqlusers',
                 'Quarantinedmails', 'Servers', 'Sites', 'Tokens']
 
-def server_transfer(mgr, name):
+def server_transfer(mgr, type_name):
+    class_for(type_name).drop_collection()
     s_dict = mgr.list_all()
     for key in s_dict:
         for d in s_dict[key]:
-            class_for(name)(**d).save()
+            class_for(type_name)(**d).save()
 
-def generic_transfer(mgr, name):
+def generic_transfer(mgr, type_name):
     """
     Standard routine to transfer _almost_ any Opalstack resource
     to a local MongoDB database.
     """
+    class_for(type_name).drop_collection()
     item_list = mgr.list_all()
     for item in item_list:
-        class_for(name)(**item).save()
+        class_for(type_name)(**item).save()
 
 specials = {'Servers': server_transfer}
 
@@ -44,10 +45,10 @@ specials = {'Servers': server_transfer}
 def main():
     api = ostack.Api(token=token)
     connect("opalstack")
-    for name in object_types:
-        print("Grabbing", name)
-        mgr = getattr(ostack.api, f"{name}Manager")(api)
-        specials.get(name, generic_transfer)(mgr, name)
+    for type_name in object_type_names:
+        print("Grabbing", type_name)
+        mgr = getattr(ostack.api, f"{type_name}Manager")(api)
+        specials.get(type_name, generic_transfer)(mgr, type_name)
 
 if __name__ == '__main__':
     main()
